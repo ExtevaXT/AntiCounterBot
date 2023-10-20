74e736c9345b30f23de2402544d41aec1b3dd16e
ExtevaXT <96924741+ExtevaXT@users.noreply.github.com>
2023-10-20 11:24:58 +0500

    Custom delays, confirmation, fix for stacked offers, refactoring
    Default price now not working

--- AntiCounterBot/Program.cs
+++ AntiCounterBot/Program.cs
@@ -1,25 +1,24 @@
 ﻿using Newtonsoft.Json;
 using System.Net;
 using System.Reflection;
-
-//Counterbitch have { price } -> cache it
-// Add item to market for {price-1}
-// Loop
-// Counterbitch instantly sets {price-2} and it have ~60s cooldown
-// Set item price as {price-3} and we are on page
-// Repeat after our ~60s cooldown
-
-// - If failed run again after 5s to not lose advantage
-// - We should be always ahead of him
-
-// After ~720 iterations counterbitch mammoth must notice
-// TODO If he re-added item to make advantage repeat from start
-// If we are first in offers stick to second {price-1} to anticounter it again
-
-
-// IMPORTANT
-// Huge vulnerability: if this bitch have > 1 bots, they will gangbang you.
-// They do not overbid theirselves so they autofind gap between cooldown.
+using static AntiCounterBot.Request;
+// TODO
+// 1. Finishing after buying
+// 2. Batch offers
+// 3. MMF feature
+// 4. Quick util for: 
+//    - removing / setting price for items 
+//    - checking lowest prices
+//    - adding items for sale
+//    - updating inventory
+// On start check inventory and check prices for offers on sale
+// Display offers and their prices, if it is not lowest display lowest price
+// commands:
+//  update. Update steam inventory
+//  add {steam_id} {price}. Add item for sale. Cache and store market id
+//  set {market_id} {price]. Set price for item offer
+//  check {steam_id}. Display lowest prices for item
+//  check all. Display lowest for your offers
 class Program
 {
     // Crutch for one time adding only TODO implement into loop
@@ -30,11 +29,19 @@ class Program
     static int delay = 15000;
     static int outranned = 0;
 
-    static int step = 1;
-    static int threshold = 4;
-    static int keepPrice = 1; // not working
+    // Runtime Initializing
     static int min_price;
     static int default_price;
+
+    // Config Initializing
+    static int step = 1;
+    static int threshold = 4;
+    static int keep_price = 1; // not working
+    static int step_incrementing = 1;
+
+    static int cooldown_delay = 60000;
+    static int default_delay = 15000;
+    static int short_delay = 5000;
 
     static string key;
     static string api;
@@ -46,57 +53,54 @@ class Program
             string path = Path.Combine(Environment.CurrentDirectory, "inventory.txt");
             Console.WriteLine("AntiCounterBot V2");
             Console.WriteLine("Fetching market inventory...");
-            string myInventory = $"{api}/my-inventory/?key={key}";
 
             HttpClient client = new HttpClient();
             client.Timeout = TimeSpan.FromSeconds(600);
-            var json = await Request(client, myInventory);
-            if (json == null)
+            Inventory? inventory = null;
+            while (inventory == null)
             {
-                Console.WriteLine("Market is dead, restart");
-                return;
+                string myInventory = $"{api}/my-inventory/?key={key}";
+                var json = await Request(client, myInventory);
+                if (json != null)
+                    inventory = JsonConvert.DeserializeObject<Inventory>(json);
             }
-            var inventory = JsonConvert.DeserializeObject<Inventory>(json);
-            if (inventory != null)
+            using (StreamWriter writer = new StreamWriter(path))
             {
-                using (StreamWriter writer = new StreamWriter(path))
+                foreach (var item in inventory.items)
                 {
-                    foreach (var item in inventory.items)
-                    {
-                        string itemText = $"{item.id}: {item.market_hash_name}";
-                        writer.WriteLine(itemText);
-                    }
+                    string itemText = $"{item.id}: {item.market_hash_name}";
+                    writer.WriteLine(itemText);
                 }
-                Console.WriteLine($"Inventory written to {path}");
-                string steamItemId = "";
-                // TODO remove validId shit from gpt
-                bool validId = false;
-                Console.WriteLine("Enter steam item id:");
-                do
+            }
+            Console.WriteLine($"Inventory written to {path}");
+            string steamItemId = "";
+            Console.WriteLine("Enter steam item id:");
+            while(itemName == null)
+            {
+                steamItemId = Console.ReadLine();
+                var item = inventory.items.Find(item => item.id == steamItemId);
+                if (item == null)
                 {
-                    steamItemId = Console.ReadLine();
-                    var item = inventory.items.Find(item => item.id == steamItemId);
-                    if (item != null)
-                    {
-                        validId = true;
-                        itemName = item.market_hash_name;
-                    }
-                    else Console.WriteLine("Invalid id, try again");
+                    Console.WriteLine("Invalid id, try again");
+                    continue;
                 }
-                while (!validId);
-                Console.WriteLine(Log("Selected: " + itemName));
-                Console.WriteLine("Default price, null to lowest:");
-                default_price = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Min price to harass:");
-                min_price = Convert.ToInt32(Console.ReadLine());
-                if(default_price < 1000 && min_price < 1000)
+                itemName = item.market_hash_name;
+            }
+            Console.WriteLine(Log("Selected: " + itemName));
+            Console.WriteLine("Default price, 0 to lowest:");
+            default_price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Min price to harass:");
+            min_price = Convert.ToInt32(Console.ReadLine());
+            if(default_price < 1000 || min_price < 1000)
+            {
                 Console.WriteLine("Are you sure?");
                 Console.ReadLine();
-                while (true) // Run indefinitely
-                {
-                    await ProcessItem(api, key, client, steamItemId, itemName);
-                    await Task.Delay(delay); // Wait for 60 seconds before the next iteration
-                }
+            }
+            // Main loop
+            while (true)
+            {
+                await ProcessItem(api, key, client, steamItemId, itemName);
+                await Task.Delay(delay); // Wait for 60 seconds before the next iteration
             }
         }
         catch (Exception ex)
@@ -117,70 +121,58 @@ class Program
         if (price <= min_price)
         {
             Console.WriteLine(Log($"{d} Reached min price, lowest: {lowest_price}"));
-            delay = 60000;
+            delay = cooldown_delay;
             return;
         }
         // If we are first skip
         if(price == lowest_price)
         {
-            delay = 15000;
+            delay = short_delay;
             return;
         }
         // Restart if they are banging us
         // If gap more than {threshold} iterations
         if (outranned >= threshold && marketItem != null)
         {
-            if(keepPrice != -1) keepPrice = price;
+            if(keep_price != -1) keep_price = price;
             string removeItem = $"{api}/set-price?key={key}&item_id={marketItem}&price=0&cur=RUB";
             json = await Request(client, removeItem);
-            if (json == null)
-            {
-                Console.WriteLine("Request denied");
-                delay = 5000;
-                return;
-            }
+            if (json == null) return;
             Console.WriteLine(Log($"{d} Item was outranned, removed"));
             marketItem = null;
-            delay = 65000;
+            // Let it cook
+            delay = cooldown_delay + short_delay;
             return;
         }
         // First iteration
         if (marketItem == null)
         {
-            if (keepPrice < 0) price = keepPrice;
+            if (keep_price < 0) price = keep_price;
             if(default_price != 0) price = default_price;
+            //if (outranned > threshold && step_incrementing > 0) step += step_incrementing;
             string addToSale = $"{api}/add-to-sale?key={key}&id={steamItemId}&price={price}&cur=RUB";
             json = await Request(client, addToSale);
-            if (json == null)
-            {
-                Console.WriteLine("Request denied");
-                delay = 5000;
-                return;
-            }
+            if (json == null) return;
             var sale = JsonConvert.DeserializeObject<AddToSale>(json);
             if (!sale.success)
             {
                 Console.WriteLine(sale.error);
+                delay = short_delay;
                 return;
             }
             marketItem = sale.item_id;
             Console.WriteLine(Log($"{d} Added item for {price}"));
-            // Here comes counterbitch
+            // Here comes counterbot
             // Let it cook
-            await Task.Delay(10000);
+            await Task.Delay(default_delay);
             await CheckPrices(client);
             outranned = 0;
         }
 
         string setPrice = $"{api}/set-price?key={key}&item_id={marketItem}&price={price}&cur=RUB";
         json = await Request(client, setPrice);
-        if (json == null)
-        {
-            Console.WriteLine("Request denied");
-            delay = 5000;
-            return;
-        }
-            
+        if (json == null) return;
+
         var response = JsonConvert.DeserializeObject<SetPrice>(json);
         if (!response.success)
         {
@@ -189,28 +181,30 @@ class Program
             // It is limited to market ~60s cooldown
             if (response.error == "too_often")
                 outranned++;
-            delay = 5000;
+            delay = short_delay;
             return;
         }
         if (price != lowest_price)
             Console.WriteLine(Log($"{d} Anticountered {lowest_price} with {price}"));
         else Console.WriteLine("Item has lowest price");
-        delay = 15000;
+        delay = default_delay;
         outranned = 0;
     }
     static async Task CheckPrices(HttpClient client)
     {
         string searchItemByName = $"{api}/search-item-by-hash-name/?key={key}&hash_name={itemName}";
         var json = await Request(client, searchItemByName);
-        if (json == null)
-            return;
+        if (json == null) return;
 
         var offers = JsonConvert.DeserializeObject<Offers>(json);
         lowest_price = offers.data.First().price;
         if (lowest_price != price) 
             price = lowest_price - step;
-        if (lowest_price == price && offers.data.Count>=2)
-            price = offers.data[1].price - step;
+        if (lowest_price == price && offers.data.Count>=2 && offers.data.First().count == 1)
+            if(default_price == 0)
+                price = offers.data[1].price - step;
+            //else price = default_price;
+        // default_price NOT WORKING, IT PLACES FIRST TIME ONLY / THEN GOES RE-ADD LOOP
     }
     static async Task<string?> Request(HttpClient client, string api)
     {
@@ -221,7 +215,9 @@ class Program
             return null;
         }
         var json = await content.Content.ReadAsStringAsync();
-        return await content.Content.ReadAsStringAsync();
+        // If market is lagging we might spam it
+        if (json == null) delay = short_delay;
+        return json;
     }
     static void Config()
     {
@@ -248,49 +244,4 @@ class Program
             writer.WriteLine(message);
         return message;
     }
-}
-// search-item-by-hash-name
-public class Offer
-{
-    public string market_hash_name { get; set; }
-    public int price { get; set; }
-    public long @class { get; set; }
-    public long instance { get; set; }
-    public int count { get; set; }
-}
-
-public class Offers
-{
-    public bool success { get; set; }
-    public string currency { get; set; }
-    public List<Offer> data { get; set; }
-}
-// my-inventory
-public class Item
-{
-    public string id { get; set; }
-    public string classid { get; set; }
-    public string instanceid { get; set; }
-    public string market_hash_name { get; set; }
-    public double market_price { get; set; }
-    public bool tradable { get; set; }
-}
-
-public class Inventory
-{
-    public bool success { get; set; }
-    public List<Item> items { get; set; }
-}
-// add-to-sale
-public class AddToSale
-{
-    public bool success { get; set; }
-    public int item_id { get; set; }
-    public string? error { get; set; }
-}
-// set-price
-public class SetPrice
-{
-    public bool success { get; set; }
-    public string? error { get; set; }
 }
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # AntiCounterBot
 
-It is same as counterbot but free and should go ahead of it on page (At least will harass it)
+Offender of official CounterBot (bots that interrupt offers for a 0.01) ~~and market~~
+
+It is same but free and should go ahead of it on page
+
+For auto trades check out https://github.com/soyware/OpenMarketClient
 
 <hr>
 
@@ -19,14 +23,29 @@ It is same as counterbot but free and should go ahead of it on page (At least wi
 - Logging in `log.txt`
 <hr>
 
-## IMPORTANT
-1. If counterbot user placed 2 bots in different time they may prevent your advantage but you still will harass them
-2. Multiprocess maybe will work
+![screenshot](screenshot.png)
+
+## Important
+1. If counterbot one user placed several bots they may prevent your advantage. It will spam re-adding
+2. Multiprocess is working
 3. Price API format: 100.00 is 10000
 
 ##  Guide:
-1. Use `config.txt` to specify market api key. Also you can specify threshold to re-add item, step of lowering price, and API url (I tested it only with tf2.tm)
+1. Use `config.txt` to specify:
+```csharp
+    int step = 1;
+    int threshold = 4; // {short_delay} times before re-adding
+    int keep_price = 1; // not working
+    int step_incrementing = 1; // not working
+
+    int cooldown_delay = 60000;
+    int default_delay = 15000;
+    int short_delay = 5000;
+
+    string key;
+    string api; // Tested only tf2.tm
+```
 2. Follow instructions in console
-3. Restart if error before main loop
-4. If error after main loop you can ignore it
+3. Each `too_often` error - {short_delay} gap between lowest and yours
+4. `BadGateway` or `InternalServerError` errors when market denies request or lagging
 
